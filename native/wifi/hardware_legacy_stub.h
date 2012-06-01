1f5c235ac6aae8420242bdd9fc7b0a7336ba2ff9
Hans-Christoph Steiner <hans@eds.org>
2012-06-01 13:09:15 -0400

    added note about hardware_legacy_stub as stand-in for the vendor supplied lib, used during building only

--- native/wifi/hardware_legacy_stub.h
+++ native/wifi/hardware_legacy_stub.h
@@ -1,3 +1,12 @@
+/*
+ * This just provides stubs to build against.  The actual functions
+ * are provided by a vendor supplied library for interfacing with the
+ * wifi hardware.  This build system does build a
+ * libhardware_legacy.so, but this is only for linking against, it is
+ * not deployed at all, since we expect every Android device to
+ * already have libhardware_legacy.so installed.
+ */
+
 #include <stdlib.h>
 int wifi_load_driver();
 int wifi_unload_driver();
