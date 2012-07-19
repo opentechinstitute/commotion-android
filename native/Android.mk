05a60e027f5618ec41a9de8eaf88a4201ed99dc3
Hans-Christoph Steiner <hans@eds.org>
2012-07-18 20:35:38 -0400

    purge all traces of Eclipse CDT and update 'make' build to work smoothly from the command line

--- native/Android.mk
+++ native/Android.mk
@@ -2,4 +2,5 @@
 include $(all-subdir-makefiles)
 
 # redirect stdout to stderr otherwise make eats it
-MAKE_EXTERNAL_LOG := $(shell make -C external 1>&2)
+# TODO this also runs when 'ndk-build clean' is called...
+#MAKE_EXTERNAL_LOG := $(shell make -C external 1>&2)
