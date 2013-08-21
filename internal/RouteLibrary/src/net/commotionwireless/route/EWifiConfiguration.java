8be939b0f05b77a77ec75c5b434d3827607a9e28
Will Hawkins <hawkinsw@opentechinstitute.org>
2013-08-21 18:23:00 -0400

    Support IP Assignment Type in WifiConfiguration

--- internal/RouteLibrary/src/net/commotionwireless/route/EWifiConfiguration.java
+++ internal/RouteLibrary/src/net/commotionwireless/route/EWifiConfiguration.java
@@ -3,20 +3,48 @@ package net.commotionwireless.route;
 import java.lang.reflect.Field;
 
 import android.net.wifi.WifiConfiguration;
+import android.util.Log;
 
 public class EWifiConfiguration {
 	private WifiConfiguration mWifiConfiguration;
 	private Field mLinkPropertiesField;
+	private Field mIpAssignmentField;
+	private Class IpAssignmentEnum;
+
+	/*
+	 * ABI - Do not modify order.
+	 */
+	public enum IpAssignmentType { 
+		STATIC,
+		DHCP,
+		UNASSIGNED
+	}
 
 	public EWifiConfiguration(WifiConfiguration configuration) {
 		mWifiConfiguration = configuration;
+		Log.d("EWifiConfiguration", "Class name: " + configuration.getClass().toString());
 		try {
+			IpAssignmentEnum = Class.forName("android.net.wifi.WifiConfiguration$IpAssignment");
 			mLinkPropertiesField = WifiConfiguration.class.getField("linkProperties");
+			mIpAssignmentField = WifiConfiguration.class.getField("ipAssignment");
 		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void setIpAssignmentType(IpAssignmentType type) {
+		Object types[] = IpAssignmentEnum.getEnumConstants();
+		try {
+			mIpAssignmentField.set(mWifiConfiguration, types[type.ordinal()]);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public Object getLinkProperties() {
 		try {
 			return mLinkPropertiesField.get(mWifiConfiguration);
@@ -27,7 +55,7 @@ public class EWifiConfiguration {
 		}
 		return null;
 	}
-	
+
 	public void setLinkProperties(RLinkProperties linkProperties) {
 		try {
 			mLinkPropertiesField.set(mWifiConfiguration, linkProperties.getNativeObject());
