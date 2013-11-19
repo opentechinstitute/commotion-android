484249628cfd773edb17d5ca7970962da9b3bbda
Will Hawkins <hawkinsw@opentechinstitute.org>
2013-11-19 16:23:01 -0500

    Add Olsrd features

--- src/net/commotionwireless/olsrd/Olsrd.java
+++ src/net/commotionwireless/olsrd/Olsrd.java
@@ -1,10 +1,86 @@
 package net.commotionwireless.olsrd;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.os.Handler;
+import android.util.Log;
+
 
 public class Olsrd {
 	static {
 		//System.loadLibrary("servald");
 		System.load("/data/data/net.commotionwireless.meshtether/app_bin/libolsrd.so");
 	}
+
+	public InputStream mInputStream = null, mErrorStream = null;
+	public boolean mStreamsAvailable = false;
+	public Object mLocker = null;
+
+	private Thread mOlsrdThread, mIoThread;
+	private Handler mHandler;
+
 	public native int main(String[] args);
+
+	public Olsrd(Handler handler) {
+		mLocker = new Object();
+		mHandler = handler;
+	}
+	public void stopMain() {
+		mOlsrdThread.interrupt();
+	}
+	public void startMain(final String[] args) {
+		mOlsrdThread = new Thread() {
+			public void run() {
+				main(args);
+			}
+		};
+		mOlsrdThread.start();
+
+		Log.d("Olsrd", "Waiting for streams to become available.");
+		synchronized (mLocker) {
+			while (!mStreamsAvailable) {
+				try {
+					mLocker.wait();
+				} catch (InterruptedException e)
+				{
+				}
+			}
+		}
+		Log.d("Olsrd", "Streams are available.");
+
+		if (mInputStream == null || mErrorStream == null) {
+			Log.e("Olsrd", "Streams not initialized!");
+		}
+		mIoThread = new Thread() {
+			public void run() {
+				BufferedReader mBr = new BufferedReader(new InputStreamReader(mInputStream), 1);
+				BufferedReader mBre = new BufferedReader(new InputStreamReader(mErrorStream), 1);
+
+				try{
+					String line;
+					do {
+						line = mBr.readLine();
+						mHandler.dispatchMessage(mHandler.obtainMessage(0, line));
+						line = mBre.readLine();
+					} while (line != null);
+				} catch (Exception e) {
+					/*
+					 * don't worry too much.
+					 */
+					Log.i("Olsrd", "Exception reading process output: " + e.toString());
+				}
+				try {
+					mBr.close();
+				} catch (IOException e) {
+					/*
+					 * don't really care.
+					 */
+				}
+			}
+		};
+		mIoThread.start();
+	}
 }
--- test/src/net/commotionwireless/meshtether/test/OlsrdTest.java
+++ test/src/net/commotionwireless/meshtether/test/OlsrdTest.java
@@ -1,31 +1,38 @@
 package net.commotionwireless.meshtether.test;
 
+import net.commotionwireless.meshtether.NativeHelper;
 import net.commotionwireless.olsrd.Olsrd;
+import android.os.Handler;
+import android.os.Message;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
 public class OlsrdTest extends AndroidTestCase {
 	private Olsrd mOlsrd; 
+	private Handler mHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			String message = msg.obj.toString();
+			Log.d("OlsrdTest", "msg: " + message);
+		}
+	};
 	public void setUp() {
-		mOlsrd = new Olsrd();
+		NativeHelper.unzipAssets(this.getContext());
+		mOlsrd = new Olsrd(mHandler);
 	}
 	
 	public void tearDown() {
-		
 	}
 	
 	public void testMain() {
-		Thread mThread = new Thread() {
-			public void run() {
-			mOlsrd.main(new String[] {"a", "b"});
-			}
-		};
-		mThread.start();
+		mOlsrd.startMain(new String[] {"-h"});
+
 		try {
 			Thread.sleep(5000);
-		} catch (InterruptedException ex) {
-			
+		} catch (InterruptedException interruptedEx) {
+			/*
+			 * eh
+			 */
 		}
-		mThread.interrupt();
+		mOlsrd.stopMain();
 	}
 }
